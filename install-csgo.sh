e7a0af502dfe32d2e1418264f27f8e4d4c8d064e
Jener Rasmussen <jener@jener.dk>
2020-12-27 20:45:07 +0100

    Fixes broken install script
    This seems to work for some indiscernible reason.

--- install-csgo.sh
+++ install-csgo.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-/usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740 validate +quit
+printf "quit\\n" | /usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740
 
 # Fix curl dependency
 #ln -s /usr/lib/i386-linux-gnu/libcurl.so.4 /usr/src/csgo/bin/libcurl.so
