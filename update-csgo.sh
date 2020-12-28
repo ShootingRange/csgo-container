de5a0219c4f0809dfe0448eb6c2855244eb8af6c
Jener Rasmussen <jener@jener.dk>
2020-12-28 14:24:07 +0100

    Apply fix to update-csgo.sh

--- update-csgo.sh
+++ update-csgo.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-/usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740 +quit
+printf "quit\\n" | /usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740
