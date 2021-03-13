d7babf23c64b94bf4facfae1065c2a6d41689a95
Jener Rasmussen <jener@jener.dk>
2021-03-13 21:50:19 +0100

    Move quit command from stdin to commandline of steamcmd

--- install-csgo.sh
+++ install-csgo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "quit\\n" | /usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740
+/usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740 +quit
 
 # Fix curl dependency
 #ln -s /usr/lib/i386-linux-gnu/libcurl.so.4 /usr/src/csgo/bin/libcurl.so
--- update-csgo.sh
+++ update-csgo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf "quit\\n" | /usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740
+/usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740 +quit
