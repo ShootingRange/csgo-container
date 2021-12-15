73a94d57a2a8c8868658360d33943dc90f082d9b
Jener Rasmussen <jener@jener.dk>
2021-12-15 18:54:34 +0100

    Fix steamcmd complaint about +login being before +force_install_dir

--- install-csgo.sh
+++ install-csgo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/src/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/src/csgo +app_update 740 +quit
+/usr/src/steamcmd/steamcmd.sh +force_install_dir /usr/src/csgo +login anonymous +app_update 740 +quit
 
 echo ">>> Finished installing CS:GO <<<"
